b809ffbf7f87e1d6629903072bc326ee7825070e
estevaog3 <eccosta@protonmail.com>
2024-10-19 23:23:49 -0400

    E disse Deus: haja luz!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="px-4 flex justify-center">
+      <div className="w-full max-w-[70ch]">
+        <h1 className="font-bold text-4xl mb-4 mt-4">Primeira postagem</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="space-y-2 text-base">
+          <p>Olá, mundo!</p>
+          <p>
+            Hoje, nesta noite de sábado, 19 de Outubro de 2024, inicio com muita
+            alegria um projeto que estava engavetado há anos: o meu blog! 😃
+          </p>
+          <p>
+            Sobre o que vou escrever? Que bom que perguntou, meu caro leitor.
+            Irei escrever sobre a vida, o universo e tudo mais rsrs. Ok, isso
+            não diz muita coisa, né (fora a minha tentativa de referência a um
+            dos livros mais legais que já li, durante um dos períodos mais
+            legais que já vivi rsrs).
+          </p>
+          <p>
+            Querido leitor, aqui vai uma lista não exaustiva de tópicos que
+            pretendo abordar:
+          </p>
+          <ul className="pb-3 list-disc pl-8">
+            <li>
+              Tecnologia, programação, design (UI/UX), ferramentas, técnicas e
+              etc;
+            </li>
+            <li>Ciência, literatura, música, matemática, filosofia;</li>
+            <li>Filmes, séries. Vídeos do YouTube e Podcasts;</li>
+            <li>
+              Fotografia, culinária (recentemente assisti MasterChef e me
+              apaixonei rsrs);
+            </li>
+            <li>
+              Bíblia, Igreja, natureza, literatura, música e redes sociais (se
+              este item não faz sentido para você, espere o próximo post, ou o
+              que vier depois dele… ainda não sei por onde vou começar hahaha);
+            </li>
+            <li>Masculinidade, casamento e afins;</li>
+            <li>
+              livros, experiências de vida, lições aprendidas e outras coisas
+              ordinárias;
+            </li>
+            <li>Idiomas, inglês, francês e como aprender idiomas.</li>
+          </ul>
+
+          <p>
+            Eu gosto de contar histórias e de imaginar histórias e situações e
+            seus possíveis desdobramentos. Então aqui você deve encontrar também
+            contos, analogias, pesquisas, curiosidades, pensamentos,
+            imaginações, poesias (talvez) e uma ou outra coisa aleatória que eu
+            esteja pensando rsrs. Também gosto de acrescentar uma pitada de
+            <span className="italic">code switching</span>, fazer trocadilhos e
+            piadas aleatórias ou não tão aleatórias. (Confissão: gente, as vezes
+            eu acho que sou doido porque faço anotações direto no celular, sem
+            nunca postar nada. Sou do tipo{" "}
+            <span className="italic">low profile</span>).
+          </p>
+          <p>
+            E, como o querido leitor já deve ter percebido, é de se esperar que
+            irei fazer digressões e falar de coisas aleatórias do nada (rsrs eu
+            acho um barato fazer isso). Tipo, eu sou do tipo de pessoa que
+            consigo conectar um Ônibus à um abajur.
+          </p>
+          <p>
+            (Deixa eu tentar improvisar agora, vamos lá, sem edição. Um Ônibus é
+            grande, um abajur é pequeno. No onibus várias pessoas entram. Estas
+            pessoas são chamadas passageiros. Elas precisam pagar para poder
+            realizar o transporte. Mas, um abajur, somente a luz sai e é de
+            Graça. Pronto. Ah! De Graça é intencional, vou explicar isto melhor
+            num post futuro. Voltando…).
+          </p>
+          <p>
+            A minha expectativa é de manter este blog durante toda minha vida
+            nesta terra, então pretendo sempre voltar de tempos em tempos para
+            escrever aqui.
+          </p>
+          <p>
+            Eu amo fazer registros e recordá-los. Aqui estarei escrevendo a
+            minha história.
+          </p>
+          <p>Mas…</p>
+          <p>
+            Eu não estou sozinho. Jesus está comigo. Então irei escrever tudo do
+            ponto de vista de uma pessoa cristã, o que eu sou. Isso se chama
+            cosmovisão cristã. Não se trata de religião, se trata de uma visão
+            de mundo que eu ganhei ao conhecer uma pessoa. Acontece, por acaso,
+            que não se trata de uma pessoa. Mas sim de uma Pessoa. Um
+            homem-Deus. *O homem-Deus.
+          </p>
+          <p>
+            Se nada disso faz sentido para você,{" "}
+            <span className="italic">keep calm</span>, apenas venha comigo nesta
+            jornada. Vai ser incrível{" "}
+            <span className="italic">
+              &quot;and I&apos;m just getting started&quot;
+            </span>
+            .
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
